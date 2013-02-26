9642b4dd99d0d9954663b23eeec8714a61a110eb
miemiedev <b31b6b82>
2013-02-26 10:15:28 +0800

    doc

--- examples/js/main.js
+++ examples/js/main.js
@@ -65,20 +65,20 @@ $(document).ready(function(){
         }},
         { title:'股票代码', name:'SECUCODE' ,width:100, align:'center', sortable: true},
         { title:'股票名称', name:'SECUABBR' ,width:100, align:'center', sortable: true},
-        { title:'今收盘', name:'CLOSINGPRICE' ,width:60, align:'right', sortable: true, renderer: fixed2},
-        { title:'涨跌幅', name:'DAYCHANGERATE' ,width:60, align:'right', sortable: true,renderer: highliht},
-        { title:'涨跌额', name:'DAYCHANGE' ,width:60, align:'right', sortable: true, renderer: highliht},
-        { title:'振幅', name:'AMPLITUDE' ,width:60, align:'right', sortable: true, renderer: fixed2percentage},
-        { title:'成交量(手)', name:'TURNOVERVOL' ,width:100, align:'right', sortable: true, renderer: function(val){
+        { title:'今收盘', name:'CLOSINGPRICE' ,width:60, align:'right',type:'number', sortable: true, renderer: fixed2},
+        { title:'涨跌幅', name:'DAYCHANGERATE' ,width:60, align:'right',type:'number', sortable: true,renderer: highliht},
+        { title:'涨跌额', name:'DAYCHANGE' ,width:60, align:'right',type:'number', sortable: true, renderer: highliht},
+        { title:'振幅', name:'AMPLITUDE' ,width:60, align:'right',type:'number', sortable: true, renderer: fixed2percentage},
+        { title:'成交量(手)', name:'TURNOVERVOL' ,width:100, align:'right',type:'number', sortable: true, renderer: function(val){
             return (val / 100).toFixed(2);
         }},
-        { title:'成交额(万)', name:'TURNOVERVAL' ,width:100, align:'right', sortable: true, renderer: function(val){
+        { title:'成交额(万)', name:'TURNOVERVAL' ,width:100, align:'right',type:'number', sortable: true, renderer: function(val){
             return (val / 10000).toFixed(2);
         }},
-        { title:'昨收盘', name:'PREVCLOSINGPRICE' ,width:60, align:'right', sortable: true, renderer: fixed2},
-        { title:'今开盘', name:'OPENINGPRICE',width:60, align:'right', sortable: true, renderer: fixed2},
-        { title:'最高价', name:'HIGHESTPRICE' ,width:60, align:'right', sortable: true, renderer: fixed2},
-        { title:'最低价', name:'LOWESTPRICE' ,width:60, align:'right', sortable: true, renderer: fixed2}
+        { title:'昨收盘', name:'PREVCLOSINGPRICE' ,width:60, align:'right',type:'number', sortable: true, renderer: fixed2},
+        { title:'今开盘', name:'OPENINGPRICE',width:60, align:'right',type:'number', sortable: true, renderer: fixed2},
+        { title:'最高价', name:'HIGHESTPRICE' ,width:60, align:'right',type:'number', sortable: true, renderer: fixed2},
+        { title:'最低价', name:'LOWESTPRICE' ,width:60, align:'right',type:'number', sortable: true, renderer: fixed2}
     ];
     //客户端排序示例
     $('#table3-1').mmGrid({
